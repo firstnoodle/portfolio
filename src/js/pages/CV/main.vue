7b3c9b0494fed3f707e4f9eb5b0060f27b8feff0
sebastian <seb@firstnoodle.io>
2020-04-02 23:08:05 +0200

    CV/main.vue: added CvItems based on cv.js

--- src/js/pages/CV/main.vue
+++ src/js/pages/CV/main.vue
@@ -1,9 +1,27 @@
 <template>
-    <h1>Curriculum Vitae</h1>
+    <div>
+        <cv-item
+            v-for="item in CV"
+            :key="item.logo"
+            :time="item.time"
+            :logo="item.logo"
+            :title="item.title"
+            :body="item.body"
+        />
+    </div>
 </template>
 
 <script>
+import CvItem from './components/CvItem.vue';
+import CV from './cv.js';
+
 export default {
     name: 'CV',
+    components: { CvItem },
+    data() {
+        return {
+            CV,
+        };
+    },
 };
 </script>
