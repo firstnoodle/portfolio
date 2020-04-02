3c94c1beeec2d3f164dc7e27905a3a7c1980a889
sebastian <seb@firstnoodle.io>
2020-04-02 23:07:27 +0200

    Created cv.js

---
+++ src/js/pages/CV/cv.js
@@ -0,0 +1,49 @@
+export default [
+    {
+        time: '2017-2020',
+        logo: 'eesea_logo',
+        title: 'UX Designer & Front End Developer',
+        body:
+            'Designing and developing the eeSea data platform, software services and data tools for in-house usage. Thorough understanding of product and full tech stack. <br>Part of product development and facilitating product vs tech communi- cation between the CEO and CTO.',
+    },
+    {
+        time: '2015-2017',
+        logo: 'mofo_logo',
+        title: 'Founder, Interaction / UX Designer, Creative Technologist',
+        body:
+            'Conceptualised and designed IOS app for reviewing medical cannabis. Built datatools for large international companies. Built physical and software prototypes for public information systems.',
+    },
+    {
+        time: '2012-2015',
+        logo: 'leapcraft_logo',
+        title: 'Interaction / UX Designer, Creative Technologist',
+        body:
+            'Worked with international clients as the only UX / interaction designer. Worked on a broad range of projects, from IOT prototypes and Big Data platforms, to webapps for treating depression and datatools for manag- ing large quantities of data. I also produced a range of concept videos and animations.',
+    },
+    {
+        time: '2011-2012',
+        logo: 'rca_logo',
+        title: '50% Masters in Design Interaction',
+        body:
+            'Design fiction. Speculative design. Synthetic biology. I studied only the first year, and chose to leave the course to go back to Denmark and start working (and start a band)',
+    },
+    {
+        time: '2010-2011',
+        logo: 'ciid_logo',
+        title: 'Freelancer and teacher at CIID',
+        body: 'Concept development. Concept video/animation. Teaching Video Prototyping Class',
+    },
+    {
+        time: '2009-2010',
+        logo: 'ciid_logo',
+        title: 'Master degree in Interaction Design',
+        body:
+            '<ul><li>User Centered design</li><li>User Experience prototyping</li><li>Graphical User Interface design</li><li>Computational Design</li><li>Service Design</li><li>Physical Computing</li><li>Wearable Technology</li><li>Velux industy project</li><li>Lego industry project</li></ul>',
+    },
+    {
+        time: '2006-2009',
+        logo: 'kadk_logo',
+        title: 'Bachelors degree in Digital Interaction',
+        body: '<ul><li>Web design</li><li>Software design</li><li>User centered design</li></ul>',
+    },
+];
