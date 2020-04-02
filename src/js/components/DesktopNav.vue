d3adcc6cf169c7f6349438efeb274f94e0c525be
sebastian <seb@firstnoodle.io>
2020-04-02 22:36:30 +0200

    Created DesktopNav

---
+++ src/js/components/DesktopNav.vue
@@ -0,0 +1,27 @@
+<template>
+    <nav id="desktop-nav">
+        <div class="">First noodle</div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'DesktopNav',
+};
+</script>
+
+<style lang="scss" scoped>
+@import '~variables';
+#desktop-nav {
+    background-color: white;
+    display: none;
+    flex: 0 0 40%;
+    height: 100vh;
+    padding: 20px;
+    width: 40%;
+
+    @include desktop {
+        display: flex;
+    }
+}
+</style>
