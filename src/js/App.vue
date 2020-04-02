6872707ff64ce56b5c3197d1d955f7d4d34584e5
sebastian <seb@firstnoodle.io>
2020-04-02 22:37:26 +0200

    App.vue basic styling

--- src/js/App.vue
+++ src/js/App.vue
@@ -1,18 +1,32 @@
 <template>
-  <div id="app">
-  	<router-view />
-  </div>
+    <div id="app">
+        <desktop-nav />
+        <main id="content">
+            <router-view />
+        </main>
+    </div>
 </template>
 
 <script>
+import DesktopNav from '~/components/DesktopNav.vue';
+
 export default {
-  name: 'app',
-  data () {
-    return {
-    }
-  }
-}
+    name: 'app',
+    components: { DesktopNav },
+    data() {
+        return {};
+    },
+};
 </script>
 
 <style lang="scss">
+#app {
+    display: flex;
+    height: 100vh;
+    width: 100vw;
+}
+#content {
+    flex: 1 1 100%;
+    overflow-y: scroll;
+}
 </style>
